9c8430c01d07a160f0d128fb36be2d4bad2a3b15
Kunlarat Lueangprasirt <Kunlarat.l@qnet.net>
2024-12-01 17:46:47 +0700

    update tf

--- .github/workflows/backend.tf
+++ .github/workflows/backend.tf
@@ -1,8 +1,15 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
   backend "azurerm" {
     resource_group_name  = "terraform-rg"
     storage_account_name = "terraformstate4develop"
     container_name       = "tfstate"
     key                  = "terraform.tfstate" # State file name
   }
+
 }
